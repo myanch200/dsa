523c7895fef012157004f82a340ecc01599f7597
Martin Yanchev <myanch200@gmail.com>
2021-01-28 10:57:54 +0000

    Adding user input and printSquare method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -23,6 +23,8 @@ public class Main {
             theCol = scanner.nextInt();
             System.out.println("Enter the number to place");
             theDigit = scanner.nextInt();
+            square[theRow][theCol] = theDigit;
+            stop = checkTheSquare(square);
         }while (!stop);
     }
 
@@ -34,6 +36,38 @@ public class Main {
             }
             System.out.println();
 
+        }
+    }
+
+    private  static boolean checkRow(int[][]matrix){
+        int row1 = matrix[0][0] +matrix[0][1] +matrix[0][2] ;
+        int row2 = matrix[1][0]+ matrix[1][1]+matrix[1][2];
+        int row3 = matrix[2][0]+ matrix[2][1]+matrix[2][2];
+        return row1 == 15 && row2 == 15 && row3 == 15;
+    }
+
+    private  static boolean checkCol(int[][]matrix){
+        int col1 = matrix[0][0] +matrix[1][0] +matrix[2][0] ;
+        int col2 = matrix[0][1]+ matrix[1][1]+matrix[2][1];
+        int col3 = matrix[0][2]+ matrix[1][2]+matrix[2][2];
+        return col1 == 15 && col2 == 15 && col3 == 15;
+    }
+
+
+    private  static  boolean checkDiagonals(int[][] matrix){
+        int diagonal1 = matrix[0][0]+matrix[1][1]+matrix[2][2];
+        int diagonal2 = matrix[2][0]+matrix[2][1]+matrix[2][2];
+        return diagonal1 == 15 && diagonal2 == 15;
+
+
+    }
+    private  static boolean checkTheSquare(int[][]matrix){
+        if(checkRow(matrix)&&checkCol(matrix)&&checkDiagonals(matrix)){
+            System.out.println("Congratulations you now have magic square");
+            return true;
+        }else{
+            System.out.println("You are getting there but still not quate a magic square");
+            return false;
         }
     }
 }
